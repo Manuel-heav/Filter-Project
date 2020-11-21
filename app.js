028380590670a397efb18e5c21889d54580b721e
Manuel-heav <Manuelheav32@gmail.com>
2020-11-21 21:14:42 +0700

    All button not working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const images = [
+const imagesArray = [
   {src: 'images/cake-1.jpeg', type: 'cake', name: 'Cake Item', price: '$5'},
   {src: 'images/cake-2.jpeg', type: 'cake', name: 'Cake Item', price: '$5'},
   {src: 'images/cake-3.jpeg', type: 'cake' , name: 'Cake Item', price: '$5'},
@@ -12,27 +12,33 @@ const images = [
   {src: 'images/sweets-2.jpeg', type: 'sweet',name: 'Sweet Item', price: '$10'},
   {src: 'images/sweets-3.jpeg', type: 'sweet',name: 'Sweet Item', price: '$15'}
 ]
-const allBtn = document.getElementById('allBtn');
-const cakesBtn = document.getElementById('cakesBtn');
-const cupcakesBtn = document.getElementById('cupcakesBtn');
-const doughnutBtn = document.getElementById('doughnutBtn');
-const sweetsBtn = document.getElementById('sweetsBtn');
-const imageContainer = document.querySelector('.img-container')
 
+const imageContainer = document.querySelector('.img-container')
+const btns = document.querySelectorAll('.button')
+const parentContainer = document.querySelector('.container')
+window.addEventListener('DOMContentLoaded', displayItems(imagesArray));
 
 // Event Listeners
-allBtn.addEventListener('click', displayAllitems);
-window.addEventListener('DOMContentLoaded', displayAllitems);
-cakesBtn.addEventListener('click', (e)=>{
-  images.forEach(function(image){
-    if(image.type === 'cake'){
-      console.log(image);
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    // console.log(e.currentTarget.dataset.name);
+    const imgCategory = imagesArray.filter(function(img){
+      const name = e.currentTarget.dataset.name
+      const type = img.type;
+      if(name === type){
+        return img;
+      }
+    });
+    if(name === 'all'){
+        displayItems(imagesArray);
+    }else{
+      displayItems(imgCategory);
     }
-  })
-})
+  });
+});
 // function
-function displayAllitems(){
-  let displayImages = images.map(function(image){
+function displayItems(imageItems){
+  let displayImages = imageItems.map(function(image){
     return `<div class="img-container">
       <img src="${image.src}" class="img" alt="">
       <h3 id='name'>${image.name}</h3>
@@ -40,6 +46,5 @@ function displayAllitems(){
     </div>`
   })
   displayImages = displayImages.join('');
-  // console.log(displayImages);
-  imageContainer.parentElement.innerHTML = displayImages;
+  parentContainer.innerHTML = displayImages;
 }
